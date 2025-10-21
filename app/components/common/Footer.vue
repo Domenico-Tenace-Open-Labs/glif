d6fed06bbefcf5b8282463dcb92a232b2fa26ecc
Domenico Tenace <domenicotenace99@gmail.com>
2025-10-21 22:14:40 +0200

    chore: small refactoring

--- app/components/common/Footer.vue
+++ app/components/common/Footer.vue
@@ -3,9 +3,9 @@
     class="w-full flex justify-center bg-primary text-surface text-center px-4 md:px-64 py-4 z-20"
   >
     <span>
-      Made with ❤️
+      Made with ❤️ by
       <NuxtLink to="https://www.github.com/DomeT99/glif" external class="underline"
-        >GitHub</NuxtLink
+        >Domenico Tenace</NuxtLink
       >
     </span>
   </footer>
